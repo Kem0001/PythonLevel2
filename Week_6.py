1f4a3c2c07100c1ab5682b9dc23a64b285c33a9b
Kem Mai <kemmai2008@gmail.com>
2024-12-13 17:46:38 -0500

    Add files via upload

---
+++ Week_6.py
@@ -0,0 +1,30 @@
+# Kem
+# Date : December 13 , 2024
+# Description : Dictionaries
+# def. of dictionary : stores info in a pair like words and its definition
+# pairs known as a key : value pairs
+# Key is a unique identifier : value is accociated data
+
+# EXAMPLE
+
+student_info = {'name' : 'Alice' , 'age' : 15 , 'grade' : '10th'}
+print('Student name :' , student_info['name'])
+
+# Add a new Key-Value pair
+
+student_info ['subject'] = 'math'
+
+# Update a value
+
+student_info ['age'] = 17
+
+# Using a dictionary (methods)
+keys = student_info()
+value = student_info()
+
+# Print updated info
+
+print('updated info :' , student_info)
+
+fav_book = {'title' : 'Case Closed Series' , 'Author' : 'Gosho Aoyama' , 'Main Character' : 'Conan / Jimmy'}
+print(fav_book)
